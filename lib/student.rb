35efa144702bc77ae75d40a9e515151c2cbb87c0
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 15:32:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,18 +30,18 @@ class Student
       self.update 
     else 
       sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS students ( 
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT 
-        )
+        INSERT INTO students(name,grade)
+        VALUES (?,?)
       SQL
+      
       DB[:conn].execute(sql, self.name, self.grade)
+      
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students"[0][0])
     end 
   end 
   
   def self.create(name,grade)
+    
     
     
 
