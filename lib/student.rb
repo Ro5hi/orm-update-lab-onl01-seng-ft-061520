86358296ebbadad28932c0b963a319bc002e81ac
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 15:33:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,23 @@ class Student
   end 
   
   def self.create(name,grade)
+    student= Student.new(name,grade)
+    student.save
+  end 
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id,name,grade)
+  end 
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ?
+    SQL
     
     
     
