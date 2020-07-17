5e979ed062f93096c70c106756e67d5039d6cea5
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-17 15:31:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,9 @@ class Student
         )
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM ")
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students"[0][0])
+    end 
+    
 
 
 end
